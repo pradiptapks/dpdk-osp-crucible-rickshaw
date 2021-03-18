7972d29b1f47e51f803ff2bdcfcfa40962059d73
Karl Rister <krister@redhat.com>
2021-03-17 19:57:23 -0500

    Move system_cpu_topology.py to toolbox project

--- discover-cpu-partitioning.py
+++ discover-cpu-partitioning.py
@@ -4,7 +4,22 @@ import argparse
 import copy
 import re
 from pathlib import Path
-from system_cpu_topology import *
+
+import sys
+import os
+TOOLBOX_HOME = os.environ.get('TOOLBOX_HOME')
+if TOOLBOX_HOME is None:
+    print("This script requires libraries that are provided by the toolbox project.")
+    print("Toolbox can be acquired from https://github.com/perftool-incubator/toolbox and")
+    print("then use 'export TOOLBOX_HOME=/path/to/toolbox' so that it can be located.")
+    exit(1)
+else:
+    p = Path(TOOLBOX_HOME) / 'python'
+    if not p.exists() or not p.is_dir():
+        print("ERROR: <TOOLBOX_HOME>/python ('%s') does not exist!" % (p))
+        exit(2)
+    sys.path.append(str(p))
+from toolbox.system_cpu_topology import *
 
 # define some global variables
 class t_global(object):
